ced2681349ef0456672dfceabc0cdc95900afdd0
Linn Thit Htoo <lth1212001@gmail.com>
2025-03-15 20:00:47 +0630

    -

--- dotnet-keycloak-oauth.Auth/Dependencies/DependencyInjectionExtensions.cs
+++ dotnet-keycloak-oauth.Auth/Dependencies/DependencyInjectionExtensions.cs
@@ -6,13 +6,22 @@ namespace dotnet_keycloak_oauth.Auth.Dependencies;
 
 public static class DependencyInjectionExtensions
 {
-    public static IServiceCollection AddDependencies(this IServiceCollection services, WebApplicationBuilder builder)
+    public static IServiceCollection AddDependencies(
+        this IServiceCollection services,
+        WebApplicationBuilder builder
+    )
     {
-        builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
-            .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
+        builder
+            .Configuration.SetBasePath(builder.Environment.ContentRootPath)
+            .AddJsonFile(
+                $"appsettings.{builder.Environment.EnvironmentName}.json",
+                optional: false,
+                reloadOnChange: true
+            )
             .AddEnvironmentVariables();
 
-        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        builder
+            .Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             .AddJwtBearer(opt =>
             {
                 opt.RequireHttpsMetadata = false;
@@ -25,45 +34,49 @@ public static class DependencyInjectionExtensions
             });
         builder.Services.AddAuthorization();
 
-
         services.AddSwaggerGen(o =>
         {
             o.CustomSchemaIds(id => id.FullName!.Replace('+', '-'));
 
-            o.AddSecurityDefinition("Keycloak", new OpenApiSecurityScheme
-            {
-                Type = SecuritySchemeType.OAuth2,
-                Flows = new OpenApiOAuthFlows
-                {
-                    Implicit = new OpenApiOAuthFlow
-                    {
-                        AuthorizationUrl = new Uri(builder.Configuration["Keycloak:AuthorizationUrl"]!),
-                        Scopes = new Dictionary<string, string>
-                    {
-                        { "openid", "openid" },
-                        { "profile", "profile" }
-                    }
-                    }
-                }
-            });
-
-            var securityRequirement = new OpenApiSecurityRequirement
-        {
-            {
+            o.AddSecurityDefinition(
+                "Keycloak",
                 new OpenApiSecurityScheme
                 {
-                    Reference = new OpenApiReference
+                    Type = SecuritySchemeType.OAuth2,
+                    Flows = new OpenApiOAuthFlows
                     {
-                        Id = "Keycloak",
-                        Type = ReferenceType.SecurityScheme
+                        Implicit = new OpenApiOAuthFlow
+                        {
+                            AuthorizationUrl = new Uri(
+                                builder.Configuration["Keycloak:AuthorizationUrl"]!
+                            ),
+                            Scopes = new Dictionary<string, string>
+                            {
+                                { "openid", "openid" },
+                                { "profile", "profile" }
+                            }
+                        }
+                    }
+                }
+            );
+
+            var securityRequirement = new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Id = "Keycloak",
+                            Type = ReferenceType.SecurityScheme
+                        },
+                        In = ParameterLocation.Header,
+                        Name = "Bearer",
+                        Scheme = "Bearer"
                     },
-                    In = ParameterLocation.Header,
-                    Name = "Bearer",
-                    Scheme = "Bearer"
-                },
-                []
-            }
-        };
+                    []
+                }
+            };
 
             o.AddSecurityRequirement(securityRequirement);
         });
