b1f8db0950c6e29f1e757c65838b4786b956b596
Linn Thit Htoo <lth1212001@gmail.com>
2025-03-15 20:00:44 +0630

    -

--- dotnet-keycloak-oauth.Auth/Dependencies/DependencyInjectionExtensions.cs
+++ dotnet-keycloak-oauth.Auth/Dependencies/DependencyInjectionExtensions.cs
@@ -2,73 +2,72 @@
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
-namespace dotnet_keycloak_oauth.Auth.Dependencies
+namespace dotnet_keycloak_oauth.Auth.Dependencies;
+
+public static class DependencyInjectionExtensions
 {
-    public static class DependencyInjectionExtensions
+    public static IServiceCollection AddDependencies(this IServiceCollection services, WebApplicationBuilder builder)
     {
-        public static IServiceCollection AddDependencies(this IServiceCollection services, WebApplicationBuilder builder)
+        builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+            .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables();
+
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(opt =>
+            {
+                opt.RequireHttpsMetadata = false;
+                opt.Audience = builder.Configuration["Authentication:Audience"];
+                opt.MetadataAddress = builder.Configuration["Authentication:MetadataAddress"]!;
+                opt.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidIssuer = builder.Configuration["Authentication:ValidIssuer"]
+                };
+            });
+        builder.Services.AddAuthorization();
+
+
+        services.AddSwaggerGen(o =>
         {
-            builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
-                .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
-                .AddEnvironmentVariables();
+            o.CustomSchemaIds(id => id.FullName!.Replace('+', '-'));
 
-            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(opt =>
-                {
-                    opt.RequireHttpsMetadata = false;
-                    opt.Audience = builder.Configuration["Authentication:Audience"];
-                    opt.MetadataAddress = builder.Configuration["Authentication:MetadataAddress"]!;
-                    opt.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidIssuer = builder.Configuration["Authentication:ValidIssuer"]
-                    };
-                });
-            builder.Services.AddAuthorization();
-
-
-            services.AddSwaggerGen(o =>
+            o.AddSecurityDefinition("Keycloak", new OpenApiSecurityScheme
             {
-                o.CustomSchemaIds(id => id.FullName!.Replace('+', '-'));
-
-                o.AddSecurityDefinition("Keycloak", new OpenApiSecurityScheme
+                Type = SecuritySchemeType.OAuth2,
+                Flows = new OpenApiOAuthFlows
                 {
-                    Type = SecuritySchemeType.OAuth2,
-                    Flows = new OpenApiOAuthFlows
+                    Implicit = new OpenApiOAuthFlow
                     {
-                        Implicit = new OpenApiOAuthFlow
-                        {
-                            AuthorizationUrl = new Uri(builder.Configuration["Keycloak:AuthorizationUrl"]!),
-                            Scopes = new Dictionary<string, string>
-                        {
-                            { "openid", "openid" },
-                            { "profile", "profile" }
-                        }
-                        }
+                        AuthorizationUrl = new Uri(builder.Configuration["Keycloak:AuthorizationUrl"]!),
+                        Scopes = new Dictionary<string, string>
+                    {
+                        { "openid", "openid" },
+                        { "profile", "profile" }
                     }
-                });
-
-                var securityRequirement = new OpenApiSecurityRequirement
-            {
-                {
-                    new OpenApiSecurityScheme
-                    {
-                        Reference = new OpenApiReference
-                        {
-                            Id = "Keycloak",
-                            Type = ReferenceType.SecurityScheme
-                        },
-                        In = ParameterLocation.Header,
-                        Name = "Bearer",
-                        Scheme = "Bearer"
-                    },
-                    []
+                    }
                 }
-            };
-
-                o.AddSecurityRequirement(securityRequirement);
             });
 
-            return services;
-        }
+            var securityRequirement = new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Id = "Keycloak",
+                        Type = ReferenceType.SecurityScheme
+                    },
+                    In = ParameterLocation.Header,
+                    Name = "Bearer",
+                    Scheme = "Bearer"
+                },
+                []
+            }
+        };
+
+            o.AddSecurityRequirement(securityRequirement);
+        });
+
+        return services;
     }
 }
