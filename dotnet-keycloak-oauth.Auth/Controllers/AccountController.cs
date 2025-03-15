3f91f1216471f0d2daf3f995113f5d201e773c4d
Linn Thit Htoo <lth1212001@gmail.com>
2025-03-15 20:00:31 +0630

    -

--- dotnet-keycloak-oauth.Auth/Controllers/AccountController.cs
+++ dotnet-keycloak-oauth.Auth/Controllers/AccountController.cs
@@ -3,20 +3,19 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 
-namespace dotnet_keycloak_oauth.Auth.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AccountController : ControllerBase
-    {
-        [Authorize]
-        [HttpGet("/users/me")]
-        public IActionResult GetUser()
-        {
-            var claimsPrincipal = HttpContext.User; // Fetch claims from context
-            var claims = claimsPrincipal.Claims.ToDictionary(c => c.Type, c => c.Value);
+namespace dotnet_keycloak_oauth.Auth.Controllers;
 
-            return Ok(claims);
-        }
+[Route("api/[controller]")]
+[ApiController]
+public class AccountController : ControllerBase
+{
+    [Authorize]
+    [HttpGet("/users/me")]
+    public IActionResult GetUser()
+    {
+        var claimsPrincipal = HttpContext.User; // Fetch claims from context
+        var claims = claimsPrincipal.Claims.ToDictionary(c => c.Type, c => c.Value);
+
+        return Ok(claims);
     }
 }
